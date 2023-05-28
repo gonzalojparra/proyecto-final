2e3e6ffad480bccb555f8699e62c285fca99c8aa
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-05-28 19:39:01 -0300

    agregado campo apellido a registro

--- app/Http/Livewire/Auth/RegistroUsuario.php
+++ app/Http/Livewire/Auth/RegistroUsuario.php
@@ -8,18 +8,30 @@ class RegistroUsuario extends Component
 {
 
     public $nombreUsuario;
-    public  $message = "error";
- 
+    public $message = "error";
+    public $reglas = [
+        'nombreUsuario' => 'required|max:20|string',
+        'email' => 'required|email|regex:/(.*)@(gmail|yahoo|protonmail)\.com/i',
+    ];
+
     public function render()
     {
         return view('livewire.auth.registro-usuario');
     }
 
-    public function updatedClientName()
-    {
-        $this->validar([
-            'nombreUsuario' => 'required|min:3|max:128|string'
-        ]);
-    }
+    public function validar(){
+        $this->validate();
+        
+        $this->reset(['open', 'nombre', 'correo']);
+        $this->emit('render');
+        $this->emit('alert', ['mensaje' => 'Se agrego correctamente']);
+    } 
+
+    // public function updatedClientName()
+    // {
+    //     $this->validar([
+    //         'nombreUsuario' => 'required|min:3|max:128|string'
+    //     ]);
+    // }
 
 }
