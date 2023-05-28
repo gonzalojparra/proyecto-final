0a277fc238336d7129233874ba365479e0388193
lucasmartin97 <lucas.martin@est.fi.uncoma.edu.ar>
2023-05-28 19:53:14 -0300

    Arreglo tabla encuentros

--- database/migrations/006_create_encuentros_table.php
+++ database/migrations/006_create_encuentros_table.php
@@ -24,9 +24,9 @@ return new class extends Migration {
             $table->foreign('id_competencia')->references('id')
               ->on('competencias');
             $table->foreign('id_competidor1')->references('id')
-            ->on('competencias');
+            ->on('competidores');
             $table->foreign('id_competidor2')->references('id')
-            ->on('competencias');
+            ->on('competidores');
         });
     }
 
