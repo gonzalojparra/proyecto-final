f49a65804caddf54490e57cd9320dcf4205c3b9d
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-05-28 18:33:03 -0300

    registro con livewire

--- app/Http/Livewire/Auth/RegistroUsuario.php
+++ app/Http/Livewire/Auth/RegistroUsuario.php
@@ -6,8 +6,20 @@ use Livewire\Component;
 
 class RegistroUsuario extends Component
 {
+
+    public $nombreUsuario;
+    public  $message = "error";
+ 
     public function render()
     {
         return view('livewire.auth.registro-usuario');
     }
+
+    public function updatedClientName()
+    {
+        $this->validar([
+            'nombreUsuario' => 'required|min:3|max:128|string'
+        ]);
+    }
+
 }
