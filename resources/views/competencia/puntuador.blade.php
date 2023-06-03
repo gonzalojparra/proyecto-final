49dfd18bcf801ba0c1fcbc544e8b3e34b311a879
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-06-03 19:47:10 -0300

    puntuador vista

--- resources/views/competencia/puntuador.blade.php
+++ resources/views/competencia/puntuador.blade.php
@@ -13,17 +13,25 @@
 </head>
 
 <body class="puntuador-body">
-    <div class="botones">
-        <div class="boton-deduccion">
-            <button type="button" class="boton-puntuador text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                <span class="deduccion">-0.3</span>
-            </button>
+    <div class="puntuador">
+        <div class="botones">
+            <div id="uno" class="boton-deduccion">
+                <button type="button" class="boton-puntuador">
+                    <span class="deduccion">-0.1</span>
+                </button>
+            </div>
+            <div id="tres" class="boton-deduccion">
+                <button type="button" class="boton-puntuador">
+                    <span class="deduccion">-0.3</span>
+                </button>
+            </div>
         </div>
-        <div  class="boton-deduccion">
-            <button type="button" class="boton-puntuador text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                <span class="deduccion">-0.1</span>
-            </button </div>>
+
+        <div class="div-puntaje">
+            <h1 class="puntaje">10</h1>
         </div>
+
+    </div>
 </body>
 
 </html>
