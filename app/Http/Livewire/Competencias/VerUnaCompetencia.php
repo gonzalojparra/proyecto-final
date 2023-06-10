b6cac63e3e1309c0cf608c0b79a8ce0887252d0e
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-06-10 18:51:03 -0300

    componente Livewire

---
+++ app/Http/Livewire/Competencias/VerUnaCompetencia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Competencias;
+
+use Livewire\Component;
+
+class VerUnaCompetencia extends Component
+{
+    public function render()
+    {
+        return view('livewire.competencias.ver-una-competencia');
+    }
+}
