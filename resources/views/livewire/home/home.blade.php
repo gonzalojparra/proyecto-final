368158eb9800b732eda6d106846b679c99c563ff
Gonzalo Parra <93682630+gonzalojparra@users.noreply.github.com>
2023-06-15 15:11:36 -0300

    Fix home view

--- resources/views/livewire/home/home.blade.php
+++ resources/views/livewire/home/home.blade.php
@@ -5,12 +5,14 @@
 
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            @if( Auth::user() )
             <a href="{{ route('inscripcion') }}" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                 <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">Nuevo</span> <span class="text-sm font-medium">Competencias regionales añadidas, inscríbete!</span>
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                 </svg>
             </a>
+            @endif
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Zen Kicks</h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Combinamos la tranquilidad de la filosofía Zen con las poderosas patadas del Taekwondo y coreografías Poomsae, destacando el equilibrio entre la paz interior y la fuerza física.</p>
             <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
