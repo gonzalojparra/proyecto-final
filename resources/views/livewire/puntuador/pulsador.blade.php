88eac2bf2a0e5f9265ab42966b1ae32acfb36e89
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-06-17 19:54:02 -0300

    vista pulsador

--- resources/views/livewire/puntuador/pulsador.blade.php
+++ resources/views/livewire/puntuador/pulsador.blade.php
@@ -1,32 +1,35 @@
 <div>
     <div class="contenedor">
-        <div class="titulo">Exactitud</div>
-        <div class="puntaje-pulsadores">
-            <div class="boton-izquierdo">
-                <div class="toggle">
-                    <input type="button" class="button">
-                    <span class="button"><span class="label">-0.1</span></span>
+        <div class="pulsador">
+            <div class="titulo">Exactitud</div>
+            <div class="puntaje-pulsadores">
+                <div class="boton-izquierdo">
+                    <div class="toggle">
+                        <input type="button" class="button">
+                        <span class="button"><span class="label">-0.1</span></span>
 
+                    </div>
                 </div>
-            </div>
-            <!-- <div class="boton-izquierdo"></div> -->
-            <div class="puntaje">10</div>
-            <div class="boton-derecho">
-                <div class="toggle">
-                    <input type="button" class="button">
-                    <span class="button"> <span class="label">-0.3</span></span>
+                <!-- <div class="boton-izquierdo"></div> -->
+                <div class="puntaje">10</div>
+                <div class="boton-derecho">
+                    <div class="toggle">
+                        <input type="button" class="button">
+                        <span class="button"> <span class="label">-0.3</span></span>
 
+                    </div>
                 </div>
+                <!-- <div class="boton-derecho"></div> -->
             </div>
-            <!-- <div class="boton-derecho"></div> -->
+            <div class="info-ronda">
+                <div class="nombre-competidor">Daniela Rodriguez</div>
+                <div class="separa-info">-</div>
+                <div class="numero-ronda">RONDA 1</div>
+            </div>
         </div>
-        <div class="info-ronda">
-            <div class="nombre-competidor">Daniela Rodriguez</div>
-            <div class="separa-info">-</div>
-            <div class="numero-ronda">RONDA 1</div>
+        <div class="no-celular">
+            <h1>MIRA DESDE EL CELU PRRO</h1>
         </div>
     </div>
-    <div class="no-celular">
-    <h1>MIRA DESDE EL CELU PRRO</h1>
-    </div>
+
 </div>
