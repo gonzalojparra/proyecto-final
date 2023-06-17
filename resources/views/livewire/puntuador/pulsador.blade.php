d90d5fac018aef8e5a462999c96cf2d99fd785e9
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-06-17 20:51:58 -0300

    vista de pulsador lista :)

--- resources/views/livewire/puntuador/pulsador.blade.php
+++ resources/views/livewire/puntuador/pulsador.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="contenedor">
-        <div class="exactitud">
+        <div class="pulsador">
             <!-- En titulo va la variable $tipoPresentacion o algo asi donde vaya el titulo correspondiente del pulsador (Exactitud o Presentacion) -->
             <div class="titulo">Exactitud</div>
             <div class="puntaje-pulsadores">
@@ -10,7 +10,14 @@
                         <span class="button"><span class="label">-0.1</span></span>
                     </div>
                 </div>
-                <div class="puntaje">10</div>
+                <div class="puntaje-boton">
+                    <div class="puntaje">10</div>
+                    <div class="boton-enviar">&nbsp;
+                        <button class="enviar">
+                            Enviar
+                        </button>
+                    </div>
+                </div>
                 <div class="boton-derecho">
                     <div class="toggle">
                         <input type="button" class="button">
@@ -23,10 +30,17 @@
                 <div class="nombre-competidor">Daniela Rodriguez</div>
                 <div class="separa-info">-</div>
                 <div class="numero-ronda">RONDA 1</div>
+                <div class="separa-info">-</div>
+                <div class="info-poomsae">TAEBECK</div>
+
             </div>
         </div>
         <div class="no-celular">
-            <h1>MIRA DESDE EL CELU PRRO</h1>
+            <div class="titulo">ERROR</div>
+            <div class="mensaje-error">
+                Por favor, utilice un teléfono movil <br>
+                de manera horizontal para utilizar el pulsador
+            </div>
         </div>
     </div>
 
