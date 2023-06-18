6d15f7867d3c346bbbf72d2845f6e6525e76effb
VictoriaPerezG <victoria.perez@est.fi.uncoma.edu.ar>
2023-06-18 19:47:47 -0300

    pantalla espera para celu

--- resources/views/livewire/competencias/pantalla-espera.blade.php
+++ resources/views/livewire/competencias/pantalla-espera.blade.php
@@ -1,14 +1,36 @@
-<div id="pantallaEspera">
-<link rel="stylesheet" href="{{ asset('css/pantallaEspera.css') }}">
+<!DOCTYPE html>
+<html lang="en">
 
-<svg class="pl" width="240" height="240" viewBox="0 0 240 240">
-	<circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
-	<circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
-	<circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-	<circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-</svg> 
-<br><br>
-<h2 class="flex">Esperando el resto de los votos...</h2>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pantalla Espera</title>
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+	@livewireStyles
 
-</div>
-<!-- <h2 class="fixed flex align-items-center justify-content-center">Esperando el resto de los votos...</h2> -->
+	<link rel="stylesheet" href="{{ asset('css/pantallaEspera.css') }}">
+</head>
+
+<body>
+	<div>
+
+		<div class="pantallaEspera">
+			<svg class="pl" width="240" height="240" viewBox="0 0 240 240">
+				<circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
+				<circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
+				<circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+				<circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+			</svg>
+			<p>Esperando el resto de los votos...</p>
+		</div>
+
+		<div class="no-celular">
+			<h1>No disponible en esta pantalla</h1>
+		</div>
+
+
+	</div>
+</body>
+
+</html>
