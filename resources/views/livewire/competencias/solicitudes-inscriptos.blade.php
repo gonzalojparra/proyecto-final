797a907ab7e7af9d61c753d401025e46a260d44e
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-06-22 21:15:44 -0300

    boton nuevo (no anda yet)

--- resources/views/livewire/competencias/solicitudes-inscriptos.blade.php
+++ resources/views/livewire/competencias/solicitudes-inscriptos.blade.php
@@ -93,6 +93,14 @@
                                     </svg>                                      
                                 </span>
                             </button>
+                            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-green-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 mt-2">
+                                        <span @popper(Ver más) class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            </svg>
+                                        </span>
+                                    </button>
                         </td>
                     </tr>
                     @endforeach
