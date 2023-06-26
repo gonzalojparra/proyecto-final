14a205dce362517d9fe0d1e529c69511e811250e
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2023-06-25 23:15:29 -0300

    subo casi finalizado

--- app/Http/Livewire/Perfil/VerResultados.php
+++ app/Http/Livewire/Perfil/VerResultados.php
@@ -4,16 +4,21 @@ namespace App\Http\Livewire\Perfil;
 
 use App\Models\Competencia;
 use App\Models\CompetenciaCompetidor;
+use App\Models\CompetenciaJuez;
 use App\Models\Pasada;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Symfony\Component\VarDumper\Exception\ThrowingCasterException;
 
 class VerResultados extends Component
 {
     public $open = false;
-    public $openResul = false;
-    public $vista;
+    public $openCompetidores = false;
+    public $posicion;
+    public $competidores;
+    public $catParticipantes;
+    public $vista = 1;
     public $resultados;
     public $inscripciones;
     public $competencias;
@@ -26,9 +31,25 @@ class VerResultados extends Component
 
     public function render()
     {
-        $this->user = Auth::user()->id;
-        $this->inscripciones = ($this->vista == 2) ? $this->obtenerInscripciones($this->user) : $this->obtenerCompetenciasParticipadas($this->user);
+        $this->inscripciones = $this->distinguirUsuario();
         return view('livewire.perfil.ver-resultados');
+    }
+
+    public function mount()
+    {
+        $this->user = Auth::user();
+    }
+
+    private function distinguirUsuario()
+    {
+        $rol = $this->user->roles()->pluck('name');
+        if ($rol[0] == 'Juez' && $rol[0] != 'Admin') {
+            $cositas = ($this->vista == 2) ? $this->obtenerInscripcionesJuez($this->user->id) : $this->obtenerCompetenciasParticipadasJuez($this->user->id);
+        }
+        if ($rol[0] == 'Competidor' && $rol[0] != 'Admin') {
+            $cositas = ($this->vista == 2) ? $this->obtenerInscripciones($this->user->id) : $this->obtenerCompetenciasParticipadas($this->user->id);
+        }
+        return $cositas;
     }
 
 
@@ -42,24 +63,6 @@ class VerResultados extends Component
     {
         $this->vista = 1;
         $this->open = true;
-    }
-
-    public function traerResultado($id)
-    {
-        $this->openResul = true;
-        $this->resultados = Pasada::where('id_competencia', $id)
-            ->join('users', 'users.id', 'pasadas.id_competidor')
-            ->join('teams', 'users.id_escuela','teams.id')
-            ->select(
-                'users.name as nombre',
-                'users.id',
-                'teams.name as escuela',
-                DB::raw('SUM(calificacion) as puntos')
-            )
-            ->groupBy('id_competidor')
-            ->orderBy('puntos', 'desc')
-            ->get();
-        //dd($resultado);
     }
 
     private function obtenerInscripciones($id)
@@ -80,15 +83,92 @@ class VerResultados extends Component
 
     private function obtenerCompetenciasParticipadas($id)
     {
-        return CompetenciaCompetidor::join('competencias', 'competencia_competidor.id_competencia', 'competencias.id')
+        $cosa = CompetenciaCompetidor::join('competencias', 'competencia_competidor.id_competencia', 'competencias.id')
+            ->join('categorias', 'categorias.id', 'competencia_competidor.id')
+            ->join('users', 'competencia_competidor.id_competidor', 'users.id')
+            ->join('graduaciones', 'graduaciones.id', 'users.id_graduacion')
             ->select(
                 'competencias.titulo as nombreCompetencia',
-                'competencias.id as idCompentecia',
+                'competencias.id as idCompetencia',
                 'competencia_competidor.aprobado as estado',
-                'competencia_competidor.created_at as fecha_inscripcion'
+                'competencia_competidor.created_at as fecha_inscripcion',
+                'categorias.nombre as nombreCategoria',
+                'graduaciones.nombre as graduacion'
             )->where('competencias.estado', '=', 5)
             ->where('id_competidor', '=', $id)
             ->orderBy('competencia_competidor.id', 'asc')
             ->get();
+
+        $this->obtenerPosicion($cosa[0]->idCompetencia);
+        return $cosa;
+    }
+
+    private function obtenerPosicion($idCompetencia)
+    {
+        $resultados = Pasada::where('id_competencia', $idCompetencia)
+            ->join('users', 'users.id', 'pasadas.id_competidor')
+            ->select(
+                'users.name as nombre',
+                'users.id',
+                DB::raw('SUM(calificacion) as puntos')
+            )
+            ->groupBy('id_competidor')
+            ->orderBy('puntos', 'desc')
+            ->get();
+        $this->catParticipantes = count($resultados);
+        $i = 0;
+        $bool = true;
+        while ($bool && $i < count($resultados)) {
+            if ($resultados[$i]->id === $this->user->id) {
+                $bool = true;
+                $this->posicion = $i + 1;
+            }
+            $i++;
+        };
+    }
+
+    private function obtenerInscripcionesJuez($id)
+    {
+        return CompetenciaJuez::where('id_juez', $id)
+            ->join('competencias', 'competencia_juez.id_competencia', 'competencias.id')
+            ->select(
+                'competencias.titulo as nombreCompetencia',
+                'competencias.fecha_inicio',
+                'competencia_juez.id as idCompentecia',
+                'competencia_juez.aprobado as estado',
+                'competencia_juez.created_at as fecha_inscripcion'
+            )
+            ->where('competencias.estado', '<', 5)
+            ->orderBy('competencia_juez.id', 'asc')
+            ->get();
+    }
+
+    private function obtenerCompetenciasParticipadasJuez($idJuez)
+    {
+        $cosas = CompetenciaJuez::join('competencias', 'competencias.id', '=', 'competencia_juez.id_competencia')
+            ->select('competencias.id as idCompentecia', 'competencias.titulo as nombreCompetencia')
+            ->where('id_juez', $idJuez)
+            ->where('competencias.estado','=',5)
+            ->get();
+
+        return $cosas;
+    }
+
+    public function mostrarCompetidoresPuntuados($idCompetencia)
+    {
+        $competidores = Pasada::join('pasadas_juez', 'pasadas.id', 'pasadas_juez.id_pasada')
+            ->join('users', 'users.id', 'pasadas.id_competidor')
+            ->select(
+                'users.name as nombre',
+                DB::raw('SUM(pasadas_juez.puntaje_exactitud) as exactitud'),
+                DB::raw('SUM(pasadas_juez.puntaje_presentacion) as presentacion')
+            )
+            ->where('pasadas_juez.id_juez', '=', $this->user->id)
+            ->where('pasadas.id_competencia', '=', $idCompetencia)
+            ->groupBy('pasadas.id_competidor')
+            ->get();
+
+           $this->competidores = $competidores;
+           $this->openCompetidores=true;
     }
 }
--- resources/views/livewire/perfil/ver-resultados.blade.php
+++ resources/views/livewire/perfil/ver-resultados.blade.php
@@ -1,9 +1,11 @@
 <div>
+
     @if ($vista == 2)
     <x-model-x-l wire:model='open' class="lg:max-w-lg bg-white">
+        @if (count($inscripciones) > 0)
         <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
             <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <th class="px-6 py-3">
+                <th class="px-6 py-3 hidden">
                     id
                 </th>
                 <th class="px-6 py-3">
@@ -12,138 +14,176 @@
                 <th class="px-6 py-3">
                     estado inscripcion
                 </th>
-                <th class="px-6 py-3">
+                <th class="px-6 py-3 sm:visible hidden">
                     fecha de inscripcion
                 </th>
-                <th class="px-6 py-3">
+                <th class="px-6 py-3 sm:visible  hidden">
                     fecha inicio
-                </th>
-            </thead>
-            <tbody>
-                @foreach ($inscripciones as $inscripcione )
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4">
-                        {{$inscripcione->idCompentecia}}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{$inscripcione->nombreCompetencia }}
-                    </td>
-                    <td class="px-6 py-4">
-                        @if ($inscripcione->estado)
-                        Aprobado
-                        @else
-                        En Proceso
-                        @endif
-                    </td>
-                    <td class="px-6 py-4">
-                        <?php $inscripcione->fecha_inscripcion = date('d/m/Y', strtotime($inscripcione->fecha_inscripcion)) ?>
-                        {{$inscripcione->fecha_inscripcion}}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{$inscripcione->fecha_inicio}}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </x-model-x-l>
-    @else
-    <x-modal wire:model='open'>
-        <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
-            <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <th class="px-6 py-3">
-                    id
-                </th>
-                <th class="px-6 py-3">
-                    Competencia
-                </th>
-                <th class="px-6 py-3">
-                    fecha de finalizacion
-                </th>
-                <th class="px-6 py-3">
-                    ver resultado
                 </th>
             </thead>
             <tbody>
                 @foreach ($inscripciones as $inscripcion )
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 hidden">
                         {{$inscripcion->idCompentecia}}
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 ">
                         {{$inscripcion->nombreCompetencia }}
                     </td>
                     <td class="px-6 py-4">
+                        @if ($inscripcion->estado)
+                        Aprobado
+                        @else
+                        En Proceso
+                        @endif
+                    </td>
+                    <td class="px-6 py-4 sm:visible  hidden">
                         <?php $inscripcion->fecha_inscripcion = date('d/m/Y', strtotime($inscripcion->fecha_inscripcion)) ?>
                         {{$inscripcion->fecha_inscripcion}}
                     </td>
-                    <td>
-                        <button class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-violet-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white" wire:click='traerResultado({{$inscripcion->idCompentecia}})'>
-                            <span class="flex justify-between items-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">
-                                    <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                                        <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                        <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z" />
-                                    </g>
-                                </svg>
-                            </span>
-                        </button>
+                    <td class="px-6 py-4 sm:visible  hidden">
+                        <?php $inscripcion->fecha_inicio = date('d/m/Y', strtotime($inscripcion->fecha_inicio)) ?>
+                        {{$inscripcion->fecha_inicio}}
                     </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
+        @else
+        <div class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 px-6 py-3 text-white">
+            <h3>
+                No hay ninguna solicitud por el momento.
+            </h3>
+        </div>
         @endif
-    </x-modal>
-    @if ($openResul == true)
-    <x-modal wire:model="openResul" style="display: flex; align-items:center;">
+    </x-model-x-l>
+    @else
+    <x-modal wire:model='open'>
+        @if (count($inscripciones) > 0)
         <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
             <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <th class="px-6 py-3">
-                    Podio
+                <th class="px-6 py-3 hidden">
+                    id
                 </th>
                 <th class="px-6 py-3">
+                    Competencia
+                </th>
+                @role('Competidor')
+                <th class="px-6 py-3">
+                    fecha de finalizacion
+                </th>
+                <th class="px-6 py-3">
+                    categoria / graduacion
+                </th>
+                <th class="px-6 py-3">
+                    puesto
+                </th>
+                @endrole
+
+                @role('Juez')
+                <th>
+                    Ver Competidores
+                </th>
+                @endrole
+            </thead>
+            <tbody>
+                @foreach ($inscripciones as $inscripcion )
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4 hidden">
+                        {{$inscripcion->idCompentecia}}
+                    </td>
+                    <td class="px-6 py-4 uppercase">
+                        {{$inscripcion->nombreCompetencia }}
+                    </td>
+                    @role('Competidor')
+                    <td class="px-6 py-4 uppercase">
+                        <?php $inscripcion->fecha_inscripcion = date('d/m/Y', strtotime($inscripcion->fecha_inscripcion)) ?>
+                        {{$inscripcion->fecha_inscripcion}}
+                    </td>
+                    <td class="px-6 py-4 uppercase">
+                        {{$inscripcion->nombreCategoria}} <b>/</b> {{$inscripcion->graduacion}}
+                    </td>
+                    <td class="px-6 py-4 uppercase">
+                        {{$posicion}}
+                    </td>
+                    @endrole
+
+                    @role('Juez')
+                    <td>
+                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-green-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 mt-2" wire:click='mostrarCompetidoresPuntuados({{$inscripcion->idCompentecia}})'>
+                            <span @popper(Ver Competidores) class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </span>
+                        </button>
+                    </td>
+                    @endrole
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @else
+        <div class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 px-6 py-3 text-white">
+            <h3>
+                No hay ningun historial por el momento.
+            </h3>
+        </div>
+        @endif
+    </x-modal>
+    @endif
+
+
+
+    @if ($openCompetidores)
+    <x-modal wire:model='openCompetidores'>
+        @if (!empty($competidores))
+        <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+            <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <th class="px-6 py-3 ">
                     Competidor
                 </th>
-                <th class="px-6 py-3">
-                    Puntos
+                <th class="px-6 py-3 sm:visible hidden">
+                    Ptos Exactitud
                 </th>
-                <th class="px-6 py-3">
-                    Escuela
+                <th class="px-6 py-3 sm:visible hidden">
+                    Ptos Presentacion
+                </th>
+                <th>
+                    Pto Total
                 </th>
             </thead>
             <tbody>
-            
-                @for ($i = 0; $i < count($resultados); $i++ )
-                <?php $eresTu = ($user == $resultados[$i]->id)?"dark:bg-white bg-gray-200 text-black" : "bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" ?> 
-                <tr class="{{$eresTu}}">
-                    <td>
-                        @switch($i)
-                        @case(0)
-                        <kbd class="px-3 py-2 mx-2 text-sm font-semibold text-white bg-blue-300 border border-gray-200 rounded-lg dark:bg-blue-600 dark:text-gray-100 dark:border-gray-500">1°</kbd>
-                        @break
-                        @case(1)
-                        <kbd class="px-3 py-2 mx-2 text-sm font-semibold text-white bg-red-300 border border-gray-200 rounded-lg dark:bg-red-600 dark:text-gray-100 dark:border-gray-500">2°</kbd>
-                        @break
-                        @case(3)
-                        <kbd class="px-3 py-2 mx-2 text-sm font-semibold text-white bg-yellow-300 border border-gray-200 rounded-lg dark:bg-yellow-600 dark:text-gray-100 dark:border-gray-500">3°</kbd>
-                        @break
-                        @endswitch
+                @foreach ($competidores as $competidor )
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4 ">
+                        {{$competidor->nombre}}
                     </td>
-                    <td class="px-6 py-4">
-                        {{$resultados[$i]->nombre}}
+                    <td class="px-6 py-4 uppercase sm:visible hidden">
+                        {{$competidor->exactitud }}
                     </td>
-                    <td class="px-6 py-4">
-                        {{$resultados[$i]->puntos }}
+                    <td class="px-6 py-4 uppercase sm:visible hidden">
+                        {{$competidor->presentacion}}
                     </td>
-                    <td> 
-                        {{$resultados[$i]->escuela}}
+                    <td class="px-6 py-4 uppercase">
+                        {{$competidor->exactitud + $competidor->presentacion}}
                     </td>
-                    </tr>
-                    @endfor
+
+                </tr>
+                @endforeach
             </tbody>
         </table>
-    </x-modal>
+        @else
+        <div class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 px-6 py-3 text-white">
+            <h3>
+                No hay ningun competidor puntuado por el momento.
+            </h3>
+        </div>
+        @endif
 
+    </x-modal>
     @endif
+
+
 </div>
